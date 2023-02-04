14d5b8be565a69fabd1b4dd477cdb9ae43cd194d
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-04 12:43:41 -0500

    created a help command

---
+++ commands/help.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Reveals all available commands and what they do."),
+  async execute(interaction) {
+    await interaction.reply(
+      `${interaction.user} has used the **/help** command! This is a list of all existing commands and what they do. \n 
+      **/help** => shows a list of all commands 
+      **/dumbot** => says a very simple message`
+    );
+  },
+};
