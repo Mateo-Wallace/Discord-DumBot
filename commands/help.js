2a9c148b81a6836900238f30f5fe603f98125f93
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-04 13:09:52 -0500

    made help secret, added timed edits to dumbot command

--- commands/help.js
+++ commands/help.js
@@ -5,10 +5,11 @@ module.exports = {
     .setName("help")
     .setDescription("Reveals all available commands and what they do."),
   async execute(interaction) {
-    await interaction.reply(
-      `${interaction.user} has used the **/help** command! This is a list of all existing commands and what they do. \n 
+    await interaction.reply({
+      content: `${interaction.user} has used the **/help** command! This is a list of all existing commands and what they do. \n 
       **/help** => shows a list of all commands 
-      **/dumbot** => says a very simple message`
-    );
+      **/dumbot** => says a very simple message`,
+      ephemeral: true,
+    });
   },
 };
