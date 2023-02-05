c26382154478d41e91ef95168e3a4acf3ad63ca6
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-05 13:59:16 -0500

    semi functioning multi roll

--- commands/roll.js
+++ commands/roll.js
@@ -33,9 +33,33 @@ module.exports = {
         }) \n **Total**: ${sum}`
       );
     } else {
-      await interaction.reply("yada");
+      let sides = messageWords[0]; // !roll 20
+      let rolls = 1;
+      if (!isNaN(messageWords[0][0] / 1) && messageWords[0].includes("d")) {
+        // !roll 4d20
+        rolls = messageWords[0].split("d")[0] / 1;
+        sides = messageWords[0].split("d")[1];
+      } else if (messageWords[0][0] == "d") {
+        // !roll d20
+        sides = sides.slice(1);
+      }
+      sides = sides / 1; // convert to number
+      if (isNaN(sides) || isNaN(rolls)) {
+        await interaction.reply(`Error executing ${interaction.commandName}`);
+      } else {
+        if (rolls > 1) {
+          const rollResults = [];
+          for (let i = 0; i < rolls; i++) {
+            rollResults.push(Math.floor(Math.random() * sides) + 1);
+          }
+          const sum = rollResults.reduce((a, b) => a + b);
+          return interaction.reply(`[${rollResults.toString()}] ${sum} ${rollFlavor}`);
+        } else {
+          return interaction.reply(
+            Math.floor(Math.random() * sides) + 1 + " " + rollFlavor
+          );
+        }
+      }
     }
-
-
   },
 };
