42ba93e2bf56c9f77100559423cec95730ee4054
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-05 12:46:40 -0500

    can roll 1d20 by typing /roll

--- commands/roll.js
+++ commands/roll.js
@@ -12,7 +12,30 @@ module.exports = {
         )
     ),
   async execute(interaction) {
-    const timeLeft = interaction.options.getString("dice");
-    await interaction.reply(`${interaction.user} \nCountdown: ${timeLeft}`);
+    const message = interaction.options.getString("dice");
+    var messageWords = [];
+    try {
+      var array = message.split(" ");
+      array.map((word) => {
+        messageWords.push(word);
+      });
+    } catch {
+      console.log(`No words`);
+    }
+    const rollFlavor = messageWords.slice(1).join(" ");
+
+    if (messageWords.length === 0) {
+      // /roll
+      const sum = Math.floor(Math.random() * 20) + 1;
+      await interaction.reply(
+        `${interaction.user} :game_die: \n **Result**: 1d20(${
+          sum == 1 || sum == 20 ? `**${sum}**` : sum
+        }) \n **Total**: ${sum}`
+      );
+    } else {
+      await interaction.reply("yada");
+    }
+
+
   },
 };
