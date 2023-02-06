014fde50256ff1000ced7efbf02a66031cd18b52
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-06 18:58:43 -0500

    added critical roll total

--- src/commands/hidden-roll.js
+++ src/commands/hidden-roll.js
@@ -32,7 +32,7 @@ module.exports = {
       await interaction.reply({
         content: `${interaction.user} :game_die: \n **Result**: 1d20 (${
           sum == 1 || sum == 20 ? `**${sum}**` : sum
-        }) \n **Total**: ${sum}`,
+        }) \n **Total**: ${sum}     **Crit Total**: ${sum * 2}`,
         ephemeral: true,
       });
     } else {
@@ -81,6 +81,7 @@ module.exports = {
       } else {
         var result = [];
         var total = [];
+        var crit = [];
         for (let i = 0; i < resultWords.length; i++) {
           const boldCrit =
             resultWords[i] == 1 ||
@@ -89,16 +90,20 @@ module.exports = {
               : resultWords[i];
           if (messageWords[i].includes("d") && !isNaN(resultWords[i] / 1)) {
             total.push(resultWords[i]);
+            crit.push(resultWords[i] * 2);
             result.push(`${messageWords[i]} (${boldCrit})`);
           } else if (resultWords[i] == "+" || resultWords[i] == "-") {
             total.push(resultWords[i]);
+            crit.push(resultWords[i]);
             result.push(`${resultWords[i]}`);
           } else if (!isNaN(resultWords[i] / 1)) {
             total.push(resultWords[i]);
+            crit.push(resultWords[i]);
             result.push(`${boldCrit}`);
           } else if (resultWords[i].constructor === Array) {
             var popped = resultWords[i].pop();
             total.push(popped);
+            crit.push(popped * 2);
             var arr = [];
             for (let j = 0; j < resultWords[i].length; j++) {
               const boldCritArr =
@@ -121,13 +126,14 @@ module.exports = {
         }
 
         const sumTotal = evaluate(total.join(" "));
+        const critTotal = evaluate(crit.join(" "));
 
         await interaction.reply({
           content: `${
             interaction.user
           } :game_die: \n **Input**: ${message} \n **Result**: ${result.join(
             " "
-          )} \n **Total**: ${sumTotal}`,
+          )} \n **Total**: ${sumTotal}     **Crit Total**: ${critTotal}`,
           ephemeral: true,
         });
       }
--- src/commands/roll.js
+++ src/commands/roll.js
@@ -30,7 +30,7 @@ module.exports = {
       await interaction.reply(
         `${interaction.user} :game_die: \n **Result**: 1d20 (${
           sum == 1 || sum == 20 ? `**${sum}**` : sum
-        }) \n **Total**: ${sum}`
+        }) \n **Total**: ${sum}     **Crit Total**: ${sum * 2}`
       );
     } else {
       var resultWords = [];
@@ -75,6 +75,7 @@ module.exports = {
       } else {
         var result = [];
         var total = [];
+        var crit = [];
         for (let i = 0; i < resultWords.length; i++) {
           const boldCrit =
             resultWords[i] == 1 ||
@@ -83,16 +84,20 @@ module.exports = {
               : resultWords[i];
           if (messageWords[i].includes("d") && !isNaN(resultWords[i] / 1)) {
             total.push(resultWords[i]);
+            crit.push(resultWords[i] * 2);
             result.push(`${messageWords[i]} (${boldCrit})`);
           } else if (resultWords[i] == "+" || resultWords[i] == "-") {
             total.push(resultWords[i]);
+            crit.push(resultWords[i]);
             result.push(`${resultWords[i]}`);
           } else if (!isNaN(resultWords[i] / 1)) {
             total.push(resultWords[i]);
+            crit.push(resultWords[i]);
             result.push(`${boldCrit}`);
           } else if (resultWords[i].constructor === Array) {
             var popped = resultWords[i].pop();
             total.push(popped);
+            crit.push(popped * 2);
             var arr = [];
             for (let j = 0; j < resultWords[i].length; j++) {
               const boldCritArr =
@@ -115,13 +120,14 @@ module.exports = {
         }
 
         const sumTotal = evaluate(total.join(" "));
+        const critTotal = evaluate(crit.join(" "));
 
         await interaction.reply(
           `${
             interaction.user
           } :game_die: \n **Input**: ${message} \n **Result**: ${result.join(
             " "
-          )} \n **Total**: ${sumTotal}`
+          )} \n **Total**: ${sumTotal}     **Crit Total**: ${critTotal}`
         );
       }
     }
