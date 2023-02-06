6e397cefe702bc1fc0da963c25f450ef2d32de1a
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-05 23:37:06 -0500

    added /roll to /help. rewrote test command

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MP2-Discord-DumBot-V2
 Trying to build dumbot again but using discord.js
+
+/insult i might be dumbot but wow your fucking retarded
+
+Groovy (probot)
+/play
+etc etc etc
--- config.js
+++ config.js
@@ -7,7 +7,8 @@ const text = {
   helpCommand: `has used the **/help** command! This is a list of all existing commands and what they do. \n 
     **/help** :arrow_right: shows a list of all commands 
     **/dumbot** :arrow_right: says a very simple message
-    **/countdown** :arrow_right: countsdown a number of seconds that the user specifies`,
+    **/countdown** :arrow_right: countsdown a number of seconds that the user specifies
+    **/roll** :arrow_right: Rolls dice. Type in the format of 4d20 + 5. The 4d20 is structured as roll 4 dice, each one with 20 sides. The + 5 is a static modifier. This can be extended as far as you'd like`,
 };
 
 module.exports = { settings, text };
