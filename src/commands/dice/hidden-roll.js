3464759ca032caf90c7a30e4b60725e842d34615
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-08 05:28:44 -0500

    dice commands fully functional, code condensed for both files to utils file

---
+++ src/commands/dice/hidden-roll.js
@@ -0,0 +1,18 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+const { diceLogic } = require("../../utils/utils");
+
+module.exports = {
+  name: "hroll",
+  description: "Rolls hidden dice based on user input.",
+  coreHelp: true,
+  options: [
+    {
+      name: "dice",
+      description: "The amount and type of dice youd like to roll plus mods",
+      type: ApplicationCommandOptionType.String,
+    },
+  ],
+  execute({ inter }) {
+    diceLogic(inter, true);
+  },
+};
