c9f9621c712d58fe9d5ef1bcaa8e1d2b3932a80d
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-09 16:06:51 -0500

    added comments to config file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   app: {
     global: 0,
     playing: "/help",
-    doubleSongError: 0,
+    doubleSongError: 0, //discord-player has an issue with skipping two songs. I've handled this by making it so when a song end the player loads the next song in queue a second time. If discord-player resolves this issue and you begin getting double songs in your queue simply turn this on.
   },
 
   opt: {
@@ -12,7 +12,7 @@ module.exports = {
       commands: [],
     },
     maxVol: 100,
-    leaveOnEnd: 0,
+    leaveOnEnd: 0, //determines if bot leaves voice after queue is finished
     loopMessage: false,
     spotifyBridge: true,
     defaultvolume: 75,
@@ -26,19 +26,22 @@ module.exports = {
 
   text: {
     helpDescription: `DumBot's code can be found at [Mateo-Wallace/DumBot](https://github.com/Mateo-Wallace/MP2-Discord-DumBot-V2) :smiling_face_with_3_hearts:\nDumBot is open source and ready for you to host yourself! \n For a full description of each command you can go to the [DumDocs](https://github.com/Mateo-Wallace/MP2-Discord-DumBot-V2/blob/main/src/assets/documentation/Usage.md)`,
-    helpCommandList: 0,
+    helpCommandList: 1, //1 is short help command, 0 is long detailed help command
   },
 
   enabledCommands: {
-    enableAll: 0, //overrides everything else in enabled commands and allows all commands
+    enableAll: 1, //overrides everything else in enabled commands and allows all commands
 
-    dumbot: 0,
+    // core commands
+    dumbot: 1,
     help: 1,
     ping: 1,
 
+    // dice commands
     hroll: 1,
     roll: 1,
 
+    // music commands
     back: 1,
     clear: 1,
     controller: 1,
