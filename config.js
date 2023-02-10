96da52a8251988bca05d8fdfa4f577342241f9d7
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-02-09 20:01:39 -0500

    turned back to local instead of global

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   app: {
-    global: 1,
+    global: 0,
     playing: "/help",
     doubleSongError: 0, //discord-player has an issue with skipping two songs. I've handled this by making it so when a song end the player loads the next song in queue a second time. If discord-player resolves this issue and you begin getting double songs in your queue simply turn this on.
   },
@@ -26,7 +26,7 @@ module.exports = {
 
   text: {
     helpDescription: `DumBot's code can be found at [Mateo-Wallace/DumBot](https://github.com/Mateo-Wallace/MP2-Discord-DumBot-V2) :smiling_face_with_3_hearts:\nDumBot is open source and ready for you to host yourself! \n For a full description of each command you can go to the [DumDocs](https://github.com/Mateo-Wallace/MP2-Discord-DumBot-V2/blob/main/src/assets/documentation/Usage.md)`,
-    helpCommandList: 1, //1 is short help command, 0 is long detailed help command
+    helpCommandList: 0, //1 is short help command, 0 is long detailed help command
   },
 
   enabledCommands: {
