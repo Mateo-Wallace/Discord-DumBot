226252dc3e34a36772aff5047292fb44023d5ba3
Mateo Wallace <mateo.t.wallace@gmail.com>
2024-08-11 14:47:44 -0400

    dry code

--- src/commands/music/loop.js
+++ src/commands/music/loop.js
@@ -1,5 +1,6 @@
-const { QueueRepeatMode } = require("discord-player");
 const { ApplicationCommandOptionType } = require("discord.js");
+// actual functionality is in repeatFunctions
+const loop = require("../../utils/repeatFunctions/loop");
 
 module.exports = {
   name: "loop",
@@ -9,12 +10,12 @@ module.exports = {
     {
       name: "action",
       description: "what action you want to preform on the loop",
-      type: ApplicationCommandOptionType.String,
+      type: ApplicationCommandOptionType.Number,
       required: true,
       choices: [
-        { name: "Queue", value: "enable_loop_queue" },
-        { name: "Disable", value: "disable_loop" },
-        { name: "Song", value: "enable_loop_song" },
+        { name: "Disable", value: 2 },
+        { name: "Song", value: 0 },
+        { name: "Queue", value: 1 },
       ],
     },
   ],
@@ -22,60 +23,7 @@ module.exports = {
   enabled: client.config.enabledCommands.loop,
 
   execute({ inter, queue }) {
-    if (!queue || !queue.node.isPlaying())
-      return inter.reply({
-        content: `No music currently playing ${inter.member}... try again ? ❌`,
-        ephemeral: true,
-      });
-
-    const methods = ["disabled", "track", "queue"];
-
-    switch (inter.options._hoistedOptions.map((x) => x.value).toString()) {
-      case "enable_loop_queue": {
-        if (queue.repeatMode === 1)
-          return inter.reply({
-            content: `You must first disable the current music in the loop mode (/loop Disable) ${inter.member}... try again ? ❌`,
-            ephemeral: true,
-          });
-
-        queue.setRepeatMode(QueueRepeatMode.QUEUE);
-        const success = methods[queue.repeatMode] === "queue";
-
-        return inter.reply({
-          content: success
-            ? `Repeat mode **enabled** the whole queue will be repeated endlessly 🔁`
-            : `Something went wrong ${inter.member}... try again ? ❌`,
-        });
-        break;
-      }
-      case "disable_loop": {
-        queue.setRepeatMode(QueueRepeatMode.OFF);
-        const success = methods[queue.repeatMode] === "disabled";
-
-        return inter.reply({
-          content: success
-            ? `Repeat mode **disabled**`
-            : `Something went wrong ${inter.member}... try again ? ❌`,
-        });
-        break;
-      }
-      case "enable_loop_song": {
-        if (queue.repeatMode === 2)
-          return inter.reply({
-            content: `You must first disable the current music in the loop mode (/loop Disable) ${inter.member}... try again ? ❌`,
-            ephemeral: true,
-          });
-
-        queue.setRepeatMode(QueueRepeatMode.TRACK);
-        const success = methods[queue.repeatMode] === "track";
-
-        return inter.reply({
-          content: success
-            ? `Repeat mode **enabled** the current song will be repeated endlessly (you can end the loop with /loop disable)`
-            : `Something went wrong ${inter.member}... try again ? ❌`,
-        });
-        break;
-      }
-    }
+    const repeatMode = inter.options.getNumber("action");
+    loop(inter, queue, repeatMode);
   },
 };
--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -1,3 +1,6 @@
+// actual functionality is in repeatFunctions
+const skip = require("../../utils/repeatFunctions/skip");
+
 module.exports = {
   name: "skip",
   description: "stop the track",
@@ -6,24 +9,6 @@ module.exports = {
   enabled: client.config.enabledCommands.skip,
 
   execute({ inter, queue }) {
-    if (!queue || !queue.node.isPlaying())
-      return inter.reply({
-        content: `No music currently playing... try again ? ❌`,
-        ephemeral: true,
-      });
-
-    if (queue.isEmpty())
-      return inter.reply({
-        content: `No next song to skip ${inter.member}... try again ? ❌`,
-        ephemeral: true,
-      });
-
-    const success = queue.node.skip();
-
-    return inter.reply({
-      content: success
-        ? `Current music ${queue.currentTrack.title} skipped ✅`
-        : `Something went wrong ${inter.member}... try again ? ❌`,
-    });
+    skip(inter, queue);
   },
 };
